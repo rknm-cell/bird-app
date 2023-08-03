1bbdf4134c6f755f7b46a6a5b55db8252771360a
rknm-cell <racknahm@gmail.com>
2023-08-03 09:47:04 -0400

    completed profile page, rendering all posts connected to profile

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,6 +3,21 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 import type { GetStaticProps, NextPage } from "next";
 import Image from "next/image";
+import { PostView } from "~/components/postview";
+
+const ProfileFeed = (props: {userId: string}) => {
+  const {data, isLoading} = api.posts.getPostsByUserId.useQuery({
+    userId: props.userId,
+  })
+
+  if (isLoading) return <LoadingPage/>
+
+  if (!data || data.length === 0) return <div>User has not posted.</div>
+
+  return <div className="flex flex-col">
+    {data.map((fullPost)=> (<PostView {...fullPost} key={fullPost.post.id}/>))}
+  </div>
+}
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data, isLoading } = api.profile.getUserByUsername.useQuery({
@@ -30,6 +45,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <div className="h-[64px]"></div>
         <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
         <div className="w-full border-b border-slate-400" />
+        <ProfileFeed userId={data.id}/>
       </PageLayout>
     </>
   );
@@ -40,6 +56,8 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
+import { LoadingPage } from "~/components/loading";
+
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
-import { SignIn, SignOutButton, useUser, SignInButton } from "@clerk/nextjs";
-import Head from "next/head";
-import Link from "next/link";
+import { useUser, SignInButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { api } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { NextPage } from "next";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import { ZodError } from "zod";
 import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 
 dayjs.extend(relativeTime);
 
@@ -79,36 +76,7 @@ const CreatePostWizard = () => {
   );
 };
 
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          {" "}
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username} `}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{` . ${dayjs(
-              post.createdAt
-            ).fromNow()}`}</span>
-          </Link>
-        </div>
-        <span className="text-2xl">{post.content}</span>
-      </div>
-    </div>
-  );
-};
 
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
